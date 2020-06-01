914fc2fb4e5f708fe46125b576cc859bbadec151
Kyle Barron <kylebarron2@gmail.com>
2020-05-31 20:42:56 -0600

    Reformat with yapf

--- dem_mosaic_tiler/handlers/app.py
+++ dem_mosaic_tiler/handlers/app.py
@@ -29,7 +29,6 @@ from dem_mosaic_tiler.utils import _get_layer_names
 from pymartini import Martini, rescale_positions
 from quantized_mesh_encoder import encode
 
-
 session = boto3_session()
 s3_client = session.client("s3")
 aws_session = AWSSession(session=session)
@@ -41,8 +40,7 @@ PIXSEL_METHODS = {
     "mean": defaults.MeanMethod,
     "median": defaults.MedianMethod,
     "stdev": defaults.StdevMethod,
-    "bdix_stdev": custom_methods.bidx_stddev,
-}
+    "bdix_stdev": custom_methods.bidx_stddev, }
 app = API(name="cogeo-mosaic-tiler")
 
 params = dict(payload_compression_method="gzip", binary_b64encode=True)
@@ -52,21 +50,21 @@ if os.environ.get("CORS"):
 
 @app.post("/create", tag=["mosaic"], **params)
 def _create(
-    body: str,
-    url: str = None,
-    minzoom: Union[str, int] = None,
-    maxzoom: Union[str, int] = None,
-    min_tile_cover: Union[str, float] = None,
-    tile_cover_sort: Union[str, bool] = False,
-    tile_format: str = None,
-    tile_scale: Union[str, int] = 1,
-    **kwargs: Any,
+        body: str,
+        url: str = None,
+        minzoom: Union[str, int] = None,
+        maxzoom: Union[str, int] = None,
+        min_tile_cover: Union[str, float] = None,
+        tile_cover_sort: Union[str, bool] = False,
+        tile_format: str = None,
+        tile_scale: Union[str, int] = 1,
+        **kwargs: Any,
 ) -> Tuple[str, str, str]:
     minzoom = int(minzoom) if isinstance(minzoom, str) else minzoom
     maxzoom = int(maxzoom) if isinstance(maxzoom, str) else maxzoom
     min_tile_cover = (
-        float(min_tile_cover) if isinstance(min_tile_cover, str) else min_tile_cover
-    )
+        float(min_tile_cover)
+        if isinstance(min_tile_cover, str) else min_tile_cover)
 
     if '{mosaicid}' in url:
         mosaicid = get_hash(
@@ -100,9 +98,7 @@ def _create(
     if tile_format in ["pbf", "mvt"]:
         tile_url = f"{app.host}/{{z}}/{{x}}/{{y}}.{tile_format}"
     elif tile_format in ["png", "jpg", "webp", "tif", "npy"]:
-        tile_url = (
-            f"{app.host}/{{z}}/{{x}}/{{y}}@{tile_scale}x.{tile_format}"
-        )
+        tile_url = (f"{app.host}/{{z}}/{{x}}/{{y}}@{tile_scale}x.{tile_format}")
     else:
         tile_url = f"{app.host}/{{z}}/{{x}}/{{y}}@{tile_scale}x"
 
@@ -118,8 +114,7 @@ def _create(
         "minzoom": meta["minzoom"],
         "name": url,
         "tilejson": "2.1.0",
-        "tiles": [tile_url],
-    }
+        "tiles": [tile_url], }
 
     return ("OK", "application/json", json.dumps(meta, separators=(",", ":")))
 
@@ -133,7 +128,9 @@ def _add(body: str, url: str) -> Tuple:
     return (
         "OK",
         "application/json",
-        json.dumps({"id": url, "status": "READY"}, separators=(",", ":")),
+        json.dumps({
+            "id": url,
+            "status": "READY"}, separators=(",", ":")),
     )
 
 
@@ -141,7 +138,7 @@ params["cache_control"] = os.environ.get("CACHE_CONTROL", None)
 
 
 @app.get("/info", tag=["metadata"], **params)
-def _info( url: str = None) -> Tuple:
+def _info(url: str = None) -> Tuple:
     """Handle /info requests."""
     if not url:
         return ("NOK", "text/plain", "Missing URL parameter")
@@ -153,12 +150,12 @@ def _info( url: str = None) -> Tuple:
             "center": meta["center"],
             "maxzoom": meta["maxzoom"],
             "minzoom": meta["minzoom"],
-            "name": url,
-        }
+            "name": url, }
 
         if not url.startswith("dynamodb://"):
             mosaic_quadkeys = set(mosaic._quadkeys)
-            tile = mercantile.quadkey_to_tile(random.sample(mosaic_quadkeys, 1)[0])
+            tile = mercantile.quadkey_to_tile(
+                random.sample(mosaic_quadkeys, 1)[0])
             assets = mosaic.tile(*tile)
             with rasterio.open(assets[0]) as src_dst:
                 layer_names = _get_layer_names(src_dst)
@@ -172,27 +169,28 @@ def _info( url: str = None) -> Tuple:
                 "Cannot retrieve 'quadkeys,layers and dtype' from dynamoDB mosaic."
             )
 
-        return ("OK", "application/json", json.dumps(response, separators=(",", ":")))
+        return (
+            "OK", "application/json", json.dumps(
+                response, separators=(",", ":")))
 
 
 @app.get("/geojson", tag=["metadata"], **params)
-def _geojson( url: str = None) -> Tuple:
+def _geojson(url: str = None) -> Tuple:
     """Handle /geojson requests."""
     if not url:
         return ("NOK", "text/plain", "Missing URL parameter")
 
     with MosaicBackend(url) as mosaic:
         geojson = {
-            "type": "FeatureCollection",
+            "type":
+                "FeatureCollection",
             "features": [
                 mercantile.feature(
-                    mercantile.quadkey_to_tile(qk), props=dict(files=files)
-                )
-                for qk, files in mosaic.mosaic_def.tiles.items()
-            ],
-        }
+                    mercantile.quadkey_to_tile(qk), props=dict(files=files))
+                for qk, files in mosaic.mosaic_def.tiles.items()], }
 
-    return ("OK", "application/json", json.dumps(geojson, separators=(",", ":")))
+    return (
+        "OK", "application/json", json.dumps(geojson, separators=(",", ":")))
 
 
 params["tag"] = ["tiles"]
@@ -200,10 +198,10 @@ params["tag"] = ["tiles"]
 
 @app.get("/tilejson.json", **params)
 def _tilejson(
-    url: str = None,
-    tile_scale: int = 1,
-    tile_format: str = None,
-    **kwargs: Any,
+        url: str = None,
+        tile_scale: int = 1,
+        tile_format: str = None,
+        **kwargs: Any,
 ) -> Tuple:
     """Handle /tilejson.json requests."""
     if not url:
@@ -232,20 +230,20 @@ def _tilejson(
             "minzoom": meta["minzoom"],
             "name": url,
             "tilejson": "2.1.0",
-            "tiles": [tile_url],
-        }
-    return ("OK", "application/json", json.dumps(response, separators=(",", ":")))
+            "tiles": [tile_url], }
+    return (
+        "OK", "application/json", json.dumps(response, separators=(",", ":")))
 
 
 @app.get("/contour/<int:z>/<int:x>/<int:y>.pbf", **params)
 def _contour(
-    z: int = None,
-    x: int = None,
-    y: int = None,
-    url: str = None,
-    tile_size: Union[str, int] = 256,
-    pixel_selection: str = "first",
-    resampling_method: str = "nearest",
+        z: int = None,
+        x: int = None,
+        y: int = None,
+        url: str = None,
+        tile_size: Union[str, int] = 256,
+        pixel_selection: str = "first",
+        resampling_method: str = "nearest",
 ) -> Tuple:
     """Handle MVT requests."""
     if not url:
@@ -254,7 +252,8 @@ def _contour(
     with MosaicBackend(url) as mosaic:
         assets = mosaic.tile(x, y, z)
         if not assets:
-            return ("EMPTY", "text/plain", f"No assets found for tile {z}-{x}-{y}")
+            return (
+                "EMPTY", "text/plain", f"No assets found for tile {z}-{x}-{y}")
 
     tile_size = int(tile_size)
 
@@ -276,31 +275,21 @@ def _contour(
 
     # TODO: shell out to gdal_contour, then to tippecanoe
 
-    return (
-        "OK",
-        "application/x-protobuf",
-        mvtEncoder(
-            tile,
-            mask,
-            band_descriptions,
-            os.path.basename(url),
-            feature_type=feature_type,
-        ),
-    )
+    return ("OK", "application/x-protobuf", "")
 
 
 @app.get("/rgb/<int:z>/<int:x>/<int:y>.<ext>", **params)
 @app.get("/rgb/<int:z>/<int:x>/<int:y>", **params)
 def _img(
-    z: int = None,
-    x: int = None,
-    y: int = None,
-    tile_size: Union[str, int] = 256,
-    ext: str = 'png',
-    url: str = None,
-    encoding: str = 'terrarium',
-    pixel_selection: str = "first",
-    resampling_method: str = "nearest",
+        z: int = None,
+        x: int = None,
+        y: int = None,
+        tile_size: Union[str, int] = 256,
+        ext: str = 'png',
+        url: str = None,
+        encoding: str = 'terrarium',
+        pixel_selection: str = "first",
+        resampling_method: str = "nearest",
 ) -> Tuple:
     """Handle tile requests."""
     if not url:
@@ -309,7 +298,8 @@ def _img(
     with MosaicBackend(url) as mosaic:
         assets = mosaic.tile(x, y, z)
         if not assets:
-            return ("EMPTY", "text/plain", f"No assets found for tile {z}-{x}-{y}")
+            return (
+                "EMPTY", "text/plain", f"No assets found for tile {z}-{x}-{y}")
 
     tile_size = int(tile_size)
 
@@ -354,13 +344,13 @@ def _img(
 @app.get("/mesh/<int:z>/<int:x>/<int:y>.terrain", **params)
 @app.get("/mesh/<int:z>/<int:x>/<int:y>@<int:scale>x.terrain", **params)
 def _mesh(
-    z: int = None,
-    x: int = None,
-    y: int = None,
-    scale: int = 1,
-    url: str = None,
-    pixel_selection: str = "first",
-    resampling_method: str = "nearest",
+        z: int = None,
+        x: int = None,
+        y: int = None,
+        scale: int = 1,
+        url: str = None,
+        pixel_selection: str = "first",
+        resampling_method: str = "nearest",
 ) -> Tuple:
     """Handle tile requests."""
     if not url:
@@ -369,7 +359,8 @@ def _mesh(
     with MosaicBackend(url) as mosaic:
         assets = mosaic.tile(x, y, z)
         if not assets:
-            return ("EMPTY", "text/plain", f"No assets found for tile {z}-{x}-{y}")
+            return (
+                "EMPTY", "text/plain", f"No assets found for tile {z}-{x}-{y}")
 
     tile_size = 256 * int(scale)
 
@@ -393,27 +384,17 @@ def _mesh(
     bounds = mercantile.bounds(mercantile.Tile(x, y, z))
 
     rescaled = rescale_positions(
-        vertices,
-        terrain,
-        tile_size=tile_size,
-        bounds=bounds,
-        flip_y=True
-    )
+        vertices, terrain, tile_size=tile_size, bounds=bounds, flip_y=True)
 
     buf = BytesIO()
     encode(buf, rescaled, triangles)
     buf.seek(0)
-    return (
-        "OK",
-        "application/vnd.quantized-mesh",
-        buf.read()
-    )
+    return ("OK", "application/vnd.quantized-mesh", buf.read())
 
 
 @app.get("/point", **params)
-def _point(
-    lng: float = None, lat: float = None, url: str = None
-) -> Tuple[str, str, str]:
+def _point(lng: float = None, lat: float = None,
+           url: str = None) -> Tuple[str, str, str]:
     """Handle point requests."""
     if not url:
         return ("NOK", "text/plain", "Missing URL parameter")
@@ -436,12 +417,12 @@ def _point(
     with rasterio.Env(aws_session):
         meta = {
             "coordinates": [lng, lat],
-            "values": [
-                {"asset": assets[ix], "values": value}
-                for ix, value in enumerate(multi_point(assets, coordinates=(lng, lat)))
-            ],
-        }
-        return ("OK", "application/json", json.dumps(meta, separators=(",", ":")))
+            "values": [{
+                "asset": assets[ix],
+                "values": value} for ix, value in enumerate(
+                    multi_point(assets, coordinates=(lng, lat)))], }
+        return (
+            "OK", "application/json", json.dumps(meta, separators=(",", ":")))
 
 
 @app.get("/favicon.ico", tag=["other"])
