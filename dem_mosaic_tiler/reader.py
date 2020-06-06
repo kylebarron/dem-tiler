a97dea0d4fdb21e651cfb937f9e092bd4133d1d6
Kyle Barron <kylebarron2@gmail.com>
2020-06-06 17:50:42 -0600

    Backfill neighboring numpy arrays

--- dem_mosaic_tiler/reader.py
+++ dem_mosaic_tiler/reader.py
@@ -1,12 +1,14 @@
+import numpy as np
 import rasterio
 from boto3.session import Session as boto3_session
+from pymartini import decode_ele
 from rasterio.session import AWSSession
 from rio_tiler.io.cogeo import tile as cogeoTiler
 from rio_tiler_mosaic.methods import defaults
 from rio_tiler_mosaic.mosaic import mosaic_tiler
 
 from cogeo_mosaic.backends import MosaicBackend
-from dem_mosaic_tiler.utils import get_aws_terrain_url
+from dem_mosaic_tiler.utils import _find_geotiff_assets, _find_terrarium_assets
 
 session = boto3_session()
 s3_client = session.client("s3")
@@ -20,6 +22,71 @@ PIXSEL_METHODS = {
     "median": defaults.MedianMethod,
     "stdev": defaults.StdevMethod}
 
+
+def find_assets(x, y, z, mosaic_url, tile_size):
+    """Find assets for input
+
+    Args:
+        - x: OSM tile index
+        - y: OSM tile index
+        - z: OSM tile index
+        - mosaic_url: either url to MosaicJSON file, or the strings "terrarium" or "geotiff" to load terrarium or geotiff tiles from AWS Terrain Tiles
+        - tile_size, one of 256, 260, 512, 516
+    """
+    if mosaic_url == 'terrarium':
+        return _find_terrarium_assets(x, y, z, tile_size)
+
+    if mosaic_url == 'geotiff':
+        return _find_geotiff_assets(x, y, z, tile_size)
+
+    with MosaicBackend(mosaic_url) as mosaic:
+        return mosaic.tile(x, y, z)
+
+
+# tile_size = 258
+# assets = _find_terrarium_assets(x, y, z, tile_size)
+# asset = assets[0]
+# data = rasterio.open(asset).read()
+# data = r.read()
+# data.shape
+# arrays = [rasterio.open(asset).read() for asset in assets]
+# arrays[0]
+# len(arrays)
+# for asset in
+# r = rasterio.open(assets)
+# data = r.read()
+# center, left, bottom, right, top = arrays
+
+
+def backfill_arrays(center, left, bottom, right, top):
+    new_shape = center.shape[0], center.shape[1] + 2, center.shape[2] + 2
+    new_arr = np.zeros(new_shape, center.dtype)
+
+    # Copy center into center of new array
+    np.copyto(new_arr[:, 1:-1, 1:-1], center)
+
+    # fill left
+    new_arr[:, 1:-1, :1] = left[:, :, -1:]
+
+    # fill right
+    new_arr[:, 1:-1, -1:] = right[:, :, :1]
+
+    # TODO: make sure these two are correct. Looks a little discontinuous
+    # fill bottom
+    new_arr[:, -1:, 1:-1] = bottom[:, :1, :]
+
+    # fill top
+    new_arr[:, :1, 1:-1] = top[:, -1:, :]
+
+    # fill corners. For now just backfill diagonally
+    new_arr[:, 0, 0] = new_arr[:, 1, 1]
+    new_arr[:, -1, 0] = new_arr[:, -2, 1]
+    new_arr[:, 0, -1] = new_arr[:, 1, -2]
+    new_arr[:, -1, -1] = new_arr[:, -2, -2]
+
+    return new_arr
+
+
 def tile_assets(
         x,
         y,
@@ -30,10 +97,10 @@ def tile_assets(
         resampling_method: str = "nearest"):
 
     if url in ['terrarium', 'geotiff']:
-        assets = get_aws_terrain_url(x, y, z, dataset='terrarium')
-    else:
-        with MosaicBackend(url) as mosaic:
-            assets = mosaic.tile(x, y, z)
+        return load_aws_terrain(x, y, z, dataset=url)
+
+    with MosaicBackend(url) as mosaic:
+        assets = mosaic.tile(x, y, z)
 
     if not assets:
         return None, None
@@ -50,3 +117,15 @@ def tile_assets(
             pixel_selection=pixsel_method(),
             resampling_method=resampling_method,
         )
+
+
+def load_aws_terrain(x, y, z, dataset):
+    assets = get_aws_terrain_url(x, y, z, dataset)
+    r = rasterio.open(assets)
+    data = r.read()
+
+    if dataset == 'terrarium':
+        data = decode_ele(data, 'terrarium')
+        decode_ele(data, 'terrarium', backfill=False).shape
+
+    return data, None
--- dem_mosaic_tiler/utils.py
+++ dem_mosaic_tiler/utils.py
@@ -6,20 +6,39 @@ from boto3.session import Session as boto3_session
 from botocore.exceptions import ClientError
 
 
-def get_aws_terrain_url(x, y, z, dataset):
-    """Get AWS Open Data url for tile
-    """
-    if dataset == 'terrarium':
-        if z >= 16:
-            return None
+def _find_terrarium_assets(x, y, z, tile_size):
+    # Terrarium has a max zoom level of 15, each tile is 256px
+    if z >= 16:
+        return None
 
-        return f's3://elevation-tiles-prod/terrarium/{z}/{x}/{y}.png'
+    base_url = 's3://elevation-tiles-prod/terrarium'
 
-    if dataset == 'geotiff':
-        if z >= 15:
-            return None
+    if tile_size == 256:
+        return [f'{base_url}/{z}/{x}/{y}.png']
 
-        return f's3://elevation-tiles-prod/terrarium/{z}/{x}/{y}.tif'
+    if tile_size == 258:
+        # center, left, bottom, right, top
+        return [
+            f'{base_url}/{z}/{x}/{y}.png',
+            f'{base_url}/{z}/{x - 1}/{y}.png',
+            f'{base_url}/{z}/{x}/{y - 1}.png',
+            f'{base_url}/{z}/{x + 1}/{y}.png',
+            f'{base_url}/{z}/{x}/{y + 1}.png']
+
+    raise NotImplementedError(f'tile_size {tile_size} not implemented')
+
+
+def _find_geotiff_assets(x, y, z, tile_size):
+    # AWS GeoTIFF tiles have a max zoom level of 14, each tile is 512px
+    if z >= 15:
+        return None
+
+    base_url = 's3://elevation-tiles-prod/geotiff'
+
+    if tile_size == 512:
+        return [f'{base_url}/{z}/{x}/{y}.png']
+
+    raise NotImplementedError(f'tile_size {tile_size} not implemented')
 
 
 def _get_layer_names(src_dst):
